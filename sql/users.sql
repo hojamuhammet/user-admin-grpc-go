77072f818da2fa1a5c1d2d333911933a30861c0e
kemal <atdayewkemal@gmail.com>
2023-09-11 16:19:08 +0500

    sql logic update

--- sql/users.sql
+++ sql/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     phone_number VARCHAR(12) NOT NULL,
     blocked BOOLEAN NOT NULL DEFAULT false,
     registration_date TIMESTAMP DEFAULT TO_TIMESTAMP(TO_CHAR(CURRENT_TIMESTAMP, 'DD-MM-YYYY HH24:MI:SS'), 'DD-MM-YYYY HH24:MI:SS')
